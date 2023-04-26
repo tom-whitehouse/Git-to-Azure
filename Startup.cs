8810db4d02c1573020069913dd71d7d2f9c67ed8
tom-whitehouse <twhitehouse389@gmail.com>
2023-04-26 15:57:04 +0100

    revert

--- Startup.cs
+++ Startup.cs
@@ -1,43 +1,30 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
-namespace MyWebApp
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        CreateHostBuilder(args).Build().Run();
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.Configure(app =>
                 {
-                    webBuilder.Configure(app =>
-                    {
-                        app.UseRouting();
+                    app.UseRouting();
 
-                        app.UseEndpoints(endpoints =>
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/", async context =>
                         {
-                            endpoints.MapGet("/", async context =>
-                            {
-                                var fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
-                                var fileInfo = fileProvider.GetFileInfo("index.html");
-                                using (var stream = fileInfo.CreateReadStream())
-                                {
-                                    context.Response.ContentType = "text/html";
-                                    await stream.CopyToAsync(context.Response.Body);
-                                }
-                            });
+                            await context.Response.WriteAsync("<html><body><h1>Hello, world!!!!</h1></body></html>");
                         });
                     });
                 });
-    }
+            });
 }
