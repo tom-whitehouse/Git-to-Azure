1f9a870560bc0a34f15534ef55728b614a16577b
tom-whitehouse <twhitehouse389@gmail.com>
2023-04-26 15:34:26 +0100

    test

--- Startup.cs
+++ Startup.cs
@@ -1,33 +1,28 @@
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                webBuilder.Configure(app =>
-                {
-                    app.UseRouting();
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+    }
+}
 
-                    app.UseEndpoints(endpoints =>
-                    {
-                        endpoints.MapGet("/", async context =>
-                        {
-                            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
-                            var htmlContent = await File.ReadAllTextAsync(filePath);
-                            await context.Response.WriteAsync(htmlContent);
-                        });
-                    });
-                });
-            });
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+    }
 }
