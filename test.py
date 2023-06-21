af237e1ea431681a34e5e43d7a08f06e1550099a
tom-whitehouse <twhitehouse389@gmail.com>
2023-06-21 10:11:22 +0100

    uppercase

--- test.py
+++ test.py
@@ -2,11 +2,11 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-first_input = 8
+FIRST_INPUT = 8
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
-squre_root = first_input ** 0.5
+SQUARE_ROOT = FIRST_INPUT ** 0.5
 print('The square root of 8 is ')
-print(squre_root)
+print(SQUARE_ROOT)
