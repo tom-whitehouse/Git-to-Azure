847e780c210f769b66bc8d2d37d4cdd5ee837167
tom-whitehouse <twhitehouse389@gmail.com>
2023-06-21 10:07:06 +0100

    lint

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
+"""Module providingFunction printing python version."""
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-number = 8 
+Number = 8
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
-number_sqrt = number ** 0.5
+Number_sqrt = Number ** 0.5
 print('The square root of 8 is ')
-print(number_sqrt)
+print(Number_sqrt)
