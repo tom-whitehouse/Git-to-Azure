c6dd0e2901a59a4ee996fae8ae9f66ec4acde3a9
tom-whitehouse <twhitehouse389@gmail.com>
2023-06-21 10:19:45 +0100

    flake8 change

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 FIRST_INPUT = 8
 
 # To take the input from the user
-#num = float(input('Enter a number: '))
+# num = float(input('Enter a number: '))
 
 SQUARE_ROOT = FIRST_INPUT ** 0.5
 print('The square root of 8 is ')
