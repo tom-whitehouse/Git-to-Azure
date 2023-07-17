c0979164c672a8ca72a7aae75bd98ac6eb84ba36
tom-whitehouse <twhitehouse389@gmail.com>
2023-07-17 09:18:08 +0100

    update

--- README.md
+++
@@ -1,8 +0,0 @@
-# Git-to-Azure
-
-1. We begin by importing the necessary namespaces to use the ASP.NET Core framework.
-2. We define a static Main method which creates a new IHostBuilder object using the CreateDefaultBuilder method and passes in the command-line arguments.
-3. The IHostBuilder object is then configured to use the WebHost defaults and a Configure method is called with an app parameter. This method configures the application's request pipeline.
-4. We then call UseRouting to enable routing middleware and MapGet to handle incoming GET requests to the root URL ("/").
-5. Inside the MapGet method, we define an asynchronous lambda function that takes in an HttpContext object and writes a simple HTML message to the response.
-6. Finally, we call Run on the IHostBuilder to start the application and listen for incoming requests.
--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ public class Program
                     {
                         endpoints.MapGet("/", async context =>
                         {
-                            await context.Response.WriteAsync("<html><body><h1>Helloe, world!!!!</h1>\n<h2>Heading</h2>\n<p>Gardening is my favourite hobby. I own a small plot of land next to our house. I cultivate gardening there. Every day, I spend half an hour gardening. After returning from my morning walk, I go to my garden with a spade and a bucket of water. I prepare the soil, prune the plants, and water them. I also use insecticides and fertilisers. My heart leaps with joy when I see the plants swaying in the wind. I feel heavenly pleasure as I watch them grow day by day.</p></body></html>");
+                            await context.Response.WriteAsync("<html><body><h1>Helloe, world!!</h1></body></html>");
                         });
                     });
                 });
